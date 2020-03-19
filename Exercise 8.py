08a07b626598fd2838b0516e0e9e37c369499a31
Cindy <xyuan35@asu.edu>
2020-03-18 18:02:56 -0700

    second commit

---
+++ Exercise 8.py
@@ -0,0 +1,82 @@
+# What does fancy_divide([0, 2, 4], 0) print out? 0,error
+def fancy_divide(numbers,index):
+    try:
+        denom = numbers[index]
+        for i in range(len(numbers)):
+            numbers[i] /= denom
+    except IndexError:
+        print("-1")
+    else:
+        print("1")
+    finally:
+        print("0")
+
+# What does fancy_divide([0, 2, 4], 4) print out? 1,0,0
+def fancy_divide(numbers, index):
+    try:
+        denom = numbers[index]
+        for i in range(len(numbers)):
+            numbers[i] /= denom
+    except IndexError:
+        fancy_divide(numbers, len(numbers) - 1)
+    except ZeroDivisionError:
+        print("-2")
+    else:
+        print("1")
+    finally:
+        print("0")
+
+# What does fancy_divide([0, 2, 4], 0) print out? 1,0,0
+def fancy_divide(numbers, index):
+    try:
+        try:
+            denom = numbers[index]
+            for i in range(len(numbers)):
+                numbers[i] /= denom
+        except IndexError:
+            fancy_divide(numbers, len(numbers) - 1)
+        else:
+            print("1")
+        finally:
+            print("0")
+    except ZeroDivisionError:
+        print("-2")
+        
+# Does this code print 0 when you call fancy_divide([0, 2, 4], 0)? No
+def fancy_divide(list_of_numbers, index):
+    try:
+        try:
+            raise Exception("0")
+        finally:
+            denom = list_of_numbers[index]
+            for i in range(len(list_of_numbers)):
+                list_of_numbers[i] /= denom
+    except Exception as ex:
+        print(ex)
+
+# Does this print 0 when you call fancy_divide([0, 2, 4], 0)? Yes
+def fancy_divide(list_of_numbers, index):
+    try:
+        try:
+            denom = list_of_numbers[index]
+            for i in range(len(list_of_numbers)):
+                list_of_numbers[i] /= denom
+        finally:
+            raise Exception("0")
+    except Exception as ex:
+        print(ex)
+
+
+## Simple Divide:
+
+def fancy_divide(list_of_numbers, index):
+   denom = list_of_numbers[index]
+   return [simple_divide(item, denom) for item in list_of_numbers]
+
+
+def simple_divide(item, denom):
+    try:
+        return item / denom
+    except ZeroDivisionError:
+        return 0
+
