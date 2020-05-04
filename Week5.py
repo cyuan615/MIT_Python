0e2fbd580de1870985991f562ce5c47d06a68d3c
Cindy <xyuan35@asu.edu>
2020-05-03 21:27:38 -0700

    05032020

--- Week5.py
+++ Week5.py
@@ -3,7 +3,7 @@
 ##############################
 # class Classname(object): Chang's example
 #     <define attributes here>
-'''
+
 import math
 
 class Coordinate(object):
@@ -118,7 +118,7 @@ s = intset()
 s.insert(5)
 print(s.member(3))
 print(s)
-'''
+
 
 ########################################
 # Why OOP
@@ -192,11 +192,89 @@ print(Eric)
 other = Person('Cindy',18)
 print(Eric.age_diff(other))
 
+import random
 
 class Student(Person):
     def __init__(self,name,age,major = None):
         Person.__init__(self,name,age)
         self.major = major
-    def change_mejor(self,major):
-        self = major
+    def change_major(self,major):
+        self.major = major
+    def speak(self):
+        r = random.random()
+        if r < 0.25:
+            print('i have homework')
+        elif 0.25 < r <= 0.5:
+            print('i need sleep')
+        elif 0.5 < r <= 0.75:
+            print('i should eat')
+        else:
+            print('i am watching TV')
+    def __str__(self):
+        return "student" + str(self.name) + ":" + str(self.age) + ":" + str(self.major)
+
+cindy = Student('Cindy',25,'cs')
+print(cindy)
+print(cindy.speak())
+cindy.change_major('ba')
+print(cindy)
+
+
+########################################
+# Class Variables:
+########################################
+class Animal(object):
+    def __init__(self,age):
+        self.age = age
+        self.name = None
+    def get_age(self):
+        return self.age
+    def get_name(self):
+        return self.name
+    def set_age(self,newage):
+        self.age = newage
+    def set_name(self,newname):
+        self.name = newname
+    def __str__(self):
+        return "animal:" + str(self.name) + ":" + str(self.age)
+
+class Rabbit(Animal):
+    tag = 1
+    def __init__(self,age,parent1 = None, parent2 = None):
+        Animal.__init__(self,age)
+        self.parent1 = parent1
+        self.parent2 = parent2
+        self.rid = Rabbit.tag
+        Rabbit.tag += 1 # tag used to give unique id to each new Rabbit instance
+    def get_rid(self):
+        return str(self.rid).zfill(3)
+    def get_parent1(self):
+        return self.parent1
+    def get_parent2(self):
+        return self.parent2
+    def __add__(self, other):
+        return Rabbit(0, self, other)
+    def __eq__(self,other):
+        parents_same = self.parent1.rid == other.parent1.rid and self.parent2.rid == other.parent2.rid
+        parents_opposite = self.parent2.rid == other.parent1.rid and self.parent1.rid == other.parent2.rid
+        return parents_opposite or parents_same
+
+
+
+peter = Rabbit(2)
+peter.set_name('Peter')
+hopsy = Rabbit(3)
+hopsy.set_name('Hopsy')
+cotton = Rabbit(1,peter,hopsy)
+# print(cotton)
+# print(cotton.get_parent1)
+
+
+mopsy = peter + hopsy   # define + operator
+mopsy.set_name('Mopsy')
+# print(mopsy.get_parent1())
+# print(mopsy.get_parent2())
+print(cotton.rid)
+print(mopsy.rid)
+print(mopsy.__eq__(cotton))
 
